4223b52cfbae488518654bd93ab02c5c58389b54
Robert <61685814+Ungetoastet@users.noreply.github.com>
2020-03-02 15:49:45 +0100

    ReactApp Created

---
+++ Mitarbeiter/my-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+ReactDOM.render(
+	<div className="Base">
+		<center>
+			<h1>Hello world!</h1>
+			<body> Test </body>
+		</center>
+	</div>,
+	document.getElementById('root')
+);
+
+console.log("Dies ist ein TestLog");
