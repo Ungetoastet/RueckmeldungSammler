63787180a6d3fdd9fed2f58e7a7a1abe937ab0d9
Robert <61685814+Ungetoastet@users.noreply.github.com>
2020-03-02 16:51:52 +0100

    Platzoptimierung - 4 classes zu einer

--- Mitarbeiter/my-app/src/index.js
+++ Mitarbeiter/my-app/src/index.js
@@ -2,43 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class NameForm extends React.Component {
+class FeedbackFormular extends React.Component {
 	render() {
 		return(
 			<form>
 				<p> Dein Name: <br />
 				<input type="text" />
 				</p>
-			</form>)
-	}
-}
-
-class RatingsForm extends React.Component {
-	render() {
-		return(
-			<form>
 				<p> Deine Stimmung: <br />
 				<input type="number" min="1" max="5"/>
 				</p>
-			</form>)
-	}
-}
-
-class CommentForm extends React.Component {
-	render() {
-		return(
-			<form>
 				<p> Sonstiges: <br />
 				<input type="text" />
 				</p>
-			</form>)
-	}
-}
-
-class SubmitButton extends React.Component {
-	render() {
-		return(
-			<form>
 				<p>
 				<input type="submit" value="absenden"/>
 				</p>
@@ -51,10 +27,7 @@ console.log("Dies ist ein TestLog");
 ReactDOM.render(
 	<div>
 		<h1> Feedback </h1>
-		<NameForm />
-		<RatingsForm />
-		<CommentForm />
-		<SubmitButton />
+		<FeedbackFormular />
 	</div>,
 	document.getElementById('root')
 );
