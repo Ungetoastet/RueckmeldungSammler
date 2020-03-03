6e26415ec4cdec4bc398b796e2dc8dc7254b8b05
Robert <61685814+Ungetoastet@users.noreply.github.com>
2020-03-03 10:17:58 +0100

    Name wird nun in die Console als Array und als Alert ausgegeben

--- Mitarbeiter/my-app/src/index.js
+++ Mitarbeiter/my-app/src/index.js
@@ -3,18 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class FeedbackFormular extends React.Component {
+	constructor(props) {
+    	super(props);
+    	this.state = {value: ''};
+    	this.handleChange = this.handleChange.bind(this);
+    	this.handleSubmit = this.handleSubmit.bind(this);
+  	}
+
+  	handleChange(event) {
+    	this.setState({value: event.target.value});
+  	}
+
+  handleSubmit(event) {
+    	alert('A name was submitted: ' + this.state.value);
+    	console.log([this.state.value])
+    	event.preventDefault();
+  	}
+
 	render() {
 		return(
-			<form>
+			<form onSubmit={this.handleSubmit}>
+				{/* Name Input */}
 				<p> Dein Name: <br />
-				<input type="text" id="name"/>
+				<input type="text" value={this.state.value} onChange={this.handleChange}/>
 				</p>
+
+				{/* Rating Input */}
 				<p> Deine Stimmung: <br />
 				<input type="number" min="1" max="5"/>
 				</p>
+
+				{/* Comment Input */}
 				<p> Sonstiges: <br />
 				<input type="text" placeholder="optional"/>
 				</p>
+
+				{/* Submit Button */}
 				<p>
 				<input type="submit" value="absenden"/>
 				</p>
@@ -29,4 +53,3 @@ ReactDOM.render(
 	</div>,
 	document.getElementById('root')
 );
-//console.log("document.getElementById("name").value");
